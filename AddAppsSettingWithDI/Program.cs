8994458bf58568f73d25ce3c97926c6f2e363eb9
Priti-Pawar <pritipawar161299@gmail.com>
2024-11-30 19:51:59 +0530

    Using Configuration using DI

---
+++ AddAppsSettingWithDI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AddAppsSettingWithDI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Create service collection for DI
+            var serviceCollection = new ServiceCollection();
+
+            // 2. Build Configuration
+            IConfiguration configuration;
+            configuration = new ConfigurationBuilder()
+                      .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                      .AddJsonFile("appsettings.json")
+                      .Build();
+
+            // 3. Add configuration to ServiceCollection
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+            serviceCollection.AddSingleton<TestConfiguration>();
+
+            // 4. Test
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var testInstance = serviceProvider.GetService<TestConfiguration>();
+            testInstance.TestMethod();
+        }
+    }
+}
---
+++ AddAppsSettingWithDI/TestConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AddAppsSettingWithDI
+{
+    public class TestConfiguration
+    {
+        public readonly IConfiguration configration;
+
+        public TestConfiguration(IConfiguration configuration)
+        {
+            this.configration = configuration;
+        }
+
+        public void TestMethod() 
+        {
+            var dataFromJsonFile = configration.GetSection("Name").Value;
+            Console.WriteLine(dataFromJsonFile);
+        }
+    }
+}
